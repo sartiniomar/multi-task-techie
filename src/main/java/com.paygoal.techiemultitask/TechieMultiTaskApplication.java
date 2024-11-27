8229d549e36cdcc96e74a537de3220a6aaa993db
sartiniomar <sartiniomar@gmail.com>
2024-11-27 18:45:07 -0300

    First Commit

---
+++ src/main/java/com.paygoal.techiemultitask/TechieMultiTaskApplication.java
@@ -0,0 +1,11 @@
+package com.paygoal.techiemultitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechieMultiTaskApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TechieMultiTaskApplication.class, args);
+	}
+}
---
+++ src/test/java/com.paygoal.techiemultitask/TechieMultiTaskApplicationTest.java
@@ -0,0 +1,11 @@
+package com.paygoal.techiemultitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechieMultiTaskApplicationTest {
+	public static void main(String[] args) {
+		SpringApplication.run(TechieMultiTaskApplicationTest.class, args);
+	}
+}
